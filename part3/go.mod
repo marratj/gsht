ed5cda921e0c62e47ab8b25c79587c1fecd094df
Marcel Juhnke <marrat@marrat.de>
2020-02-01 16:40:20 +0100

    added prometheus & healthz endpoint

--- part3/go.mod
+++ part3/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/anvie/port-scanner v0.0.0-20180225151059-8159197d3770
+	github.com/prometheus/client_golang v1.4.0 // indirect
 	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
 )
--- part3/main.go
+++ part3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -13,7 +14,28 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+
+var (
+	openPorts = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "portscanner_open_ports_total",
+			Help: "The total amount of open ports found",
+		},
+		[]string{"service"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(openPorts)
+}
 
 func main() {
 
@@ -51,37 +73,54 @@ func main() {
 		log.Printf("Error while creating clientset: %s", err.Error())
 	}
 
-	namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
-	if err != nil {
-		log.Printf("Could not find namespaces: %s", err.Error())
-	}
+	go func() {
+		for {
 
-	var podList []v1.Pod
+			namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
+			if err != nil {
+				log.Printf("Could not find namespaces: %s", err.Error())
+			}
 
-	for _, namespace := range namespaces.Items {
-		pods, err := clientset.CoreV1().Pods(namespace.ObjectMeta.Name).List(metav1.ListOptions{})
-		if err != nil {
-			log.Printf("Could not find pods: %s", err.Error())
+			var podList []v1.Pod
+
+			for _, namespace := range namespaces.Items {
+				pods, err := clientset.CoreV1().Pods(namespace.ObjectMeta.Name).List(metav1.ListOptions{})
+				if err != nil {
+					log.Printf("Could not find pods: %s", err.Error())
+				}
+				podList = append(podList, pods.Items...)
+			}
+
+			for _, pod := range podList {
+				log.Printf("Pod: %s - IP to scan: %s", pod.ObjectMeta.Name, pod.Status.PodIP)
+
+				// scan Pod with a 2 second timeout per port in 10 concurrent threads
+				ps := portscanner.NewPortScanner(pod.Status.PodIP, 2*time.Second, 10)
+
+				// get opened port
+				log.Printf("scanning port %d-%d...\n", 8000, 10000)
+
+				openedPorts := ps.GetOpenedPort(8000, 10000)
+
+				for i := 0; i < len(openedPorts); i++ {
+					port := openedPorts[i]
+					openPorts.With(prometheus.Labels{"service": "blarb"}).Add(1)
+					log.Print(" ", port, " [open]")
+				}
+			}
 		}
-		podList = append(podList, pods.Items...)
-	}
+	}()
 
-	for _, pod := range podList {
-		log.Printf("Pod: %s - IP to scan: %s", pod.ObjectMeta.Name, pod.Status.PodIP)
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "app is alive")
+	})
 
-		// scan Pod with a 2 second timeout per port in 10 concurrent threads
-		ps := portscanner.NewPortScanner(pod.Status.PodIP, 2*time.Second, 10)
-
-		// get opened port
-		log.Printf("scanning port %d-%d...\n", 8000, 10000)
-
-		openedPorts := ps.GetOpenedPort(8000, 10000)
-
-		for i := 0; i < len(openedPorts); i++ {
-			port := openedPorts[i]
-			log.Print(" ", port, " [open]")
-		}
-	}
+	// Expose the registered metrics via HTTP.
+	http.Handle("/metrics", promhttp.HandlerFor(
+		prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{},
+	))
+	log.Fatal(http.ListenAndServe(*addr, nil))
 
 }
 
